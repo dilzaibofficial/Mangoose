b89bda50ec4246899e5a3af2cd9332ca8bfcbc32
DIL ZAIB <dilzaib5848@gmail.com>
2022-10-04 10:56:22 -0700

    third

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("hello")
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/e-comm')
+
+const productSchema = mongoose.Schema({
+    name: String
+});
+
+const saveInDb = async () => {
+    let product = mongoose.model('products', productSchema)
+    let data = new product({ name: "DIL5848" })
+    return await data.save()
+}
+
+
+const updateInDb=async()=>{
+    const product=mongoose.model('products',productSchema)
+    let data=await product.updateOne(
+        {name:"Oppo F17"},
+        {$set:{price:300}}
+    )
+    
+}
+
+updateInDb()
